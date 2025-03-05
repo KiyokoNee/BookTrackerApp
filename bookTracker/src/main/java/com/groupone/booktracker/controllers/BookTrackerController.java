a4f0732ffec40d432121dc7205f98f20794a2144
KiyokoNee <k.c.gearing@gmail.com>
2025-03-05 11:35:46 -0600

    Added the key property to BorrowedBook. Created basic paths for features in the controller

--- bookTracker/src/main/java/com/groupone/booktracker/controllers/BookTrackerController.java
+++ bookTracker/src/main/java/com/groupone/booktracker/controllers/BookTrackerController.java
@@ -1,7 +1,10 @@
 package com.groupone.booktracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +17,7 @@ public class BookTrackerController {
 		return "index.jsp";
 	}
 
+	// Get requests - In Progress
 	@GetMapping
 	public String home() {
 		
@@ -29,4 +33,53 @@ public class BookTrackerController {
 	public String loginForm() {
 		return "index.jsp";
 	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "index.jsp";
+	}
+	
+	@GetMapping("/mybooks")
+	public String myBooks() {
+		return "index.jsp";
+	}
+	
+	@GetMapping("/book/{key}")
+	public String bookDetails() {
+		return "index.jsp";
+	}
+	
+	@GetMapping("/book/{key}/edit")
+	public String editBookForm() {
+		return "index.jsp";
+	}
+	
+	// Post Requests - In Progress
+	@PostMapping("/login")
+	public String login() {
+		return "redirect:/";
+	}
+	
+	@PostMapping("/register")
+	public String register() {
+		return "redirect:/";
+	}
+	
+	@PostMapping("/borrow/{key}")
+	public String borrow() {
+		return "redirect:/";
+	}
+	
+	// Put Requests - In Progress
+	@PutMapping("/book/edit")
+	public String editBorrow() {
+		return "redirect:/";
+	}
+	
+	// Delete Requests - In Progress
+	@DeleteMapping("/book/{key}/delete")
+	public String deleteBorrow() {
+		return "redirect:/";
+	}
+	
 }
